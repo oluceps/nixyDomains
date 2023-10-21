764b5fa0356de33e7d3b247a23a60507d132ac89
oluceps <i@oluceps.uk>
2023-10-21 23:08:55 +0800

    + overlay

--- flake.nix
+++ flake.nix
@@ -11,9 +11,16 @@
       ];
       systems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" "x86_64-darwin" ];
       perSystem = { config, self', inputs', pkgs, system, ... }: {
-        packages.default = with pkgs; stdenv.mkDerivation {
-          name = "domains";
-          src = ./assets;
+        packages.default =
+          with pkgs; stdenv.mkDerivation {
+            name = "domains";
+            src = ./assets;
+          };
+      };
+
+      flake = {
+        overlays.default = final: prev: {
+          nixy-domains = inputs.self.packages.x86_64-linux.default;
         };
       };
     };
